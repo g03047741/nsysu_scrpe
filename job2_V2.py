061a7aed3347b8d13f2922d9f39dcd938cabe3ec
Shu-Hong-Li <shliywork@gmail.com>
2025-05-14 16:59:09 +0800

    updated

--- job2_V2.py
+++ job2_V2.py
@@ -30,24 +30,22 @@ if os.path.exists(result_path):
 else:
     print(f"檔案 '{result_path}' 不存在。")
 
+
+low_level = 0.33
+medium_level = 0.66
+
 # 模型準備
 model = SentenceTransformer('sentence-transformers/distiluse-base-multilingual-cased-v2')
 
-risk_similarity = pd.read_csv("risk_similarity.csv", encoding="utf-8-sig")
+
 
 # 2. 建立 onets list
-onets = []
-
-for index, row in risk_similarity.iterrows():
-    soc_code = row["SOC_code"]
-    title = row["職業名稱"]
-    task = row["任務描述"]
-    risk = row["自動化風險分數"]
-    
-    # 只要確保這些欄位都有值，才加入
-    if pd.notnull(soc_code) and pd.notnull(title) and pd.notnull(task) and pd.notnull(risk):
-        onets.append((soc_code, title, task, risk))
-
+risk_similarity = pd.read_csv("risk_similarity.csv", encoding="utf-8-sig")
+risk_similarity.dropna()
+risk_similarity = risk_similarity.rename(columns={"SOC_code": "soc_code", "職業名稱": "title", "任務描述": "task", "自動化風險分數": "risk"})
+risk_similarity = risk_similarity.drop_duplicates(subset=['task'], keep='first')
+risk_similarity = risk_similarity[["soc_code", "title", "task", "risk"]]
+onets = tuple(risk_similarity.values.tolist())
 
 # 3. 確認結果
 print(f"成功建立 {len(onets)} 筆資料到 onets")
@@ -60,23 +58,20 @@ print(onets[:3])  # 顯示前3筆看一下
 # task_embeddings_en = model.encode(tasks, convert_to_tensor=True)
 
 risk_similarity_104 = pd.read_csv("104.csv", encoding="utf-8-sig")
+risk_similarity_104 = risk_similarity_104.drop_duplicates(subset=['職缺名稱', '公司', '工作內容'], keep='first')
+data_104 = risk_similarity_104.values.tolist()
 
-Data104 = risk_similarity_104.values.tolist()
 
+for row in data_104:
 
-for j in Data104:
-
-    print(j[0],j[1])
     random_uuid = uuid.uuid4()
-
-    # UUID 物件
-    # print(f"UUID 物件: {random_uuid}")
 
     # 通常會將 UUID 轉換成字串來使用
     uuid_string = str(random_uuid)
     # print(f"UUID 字串: {uuid_string}")
     # 中文職缺摘要文字
-    job_desc = j[9]
+    job_desc = row[9]
+    # print(row[9])
 
     # 假設 onets 是已經建好的清單
     # onets = [(SOC_code, title, task, risk_score), ...]
@@ -100,7 +95,7 @@ for j in Data104:
 
     for i, (code, title, task, risk) in enumerate(onets):
         sim = similarity_scores[i]
-        if round(sim, 3) >= 0.35:
+        if round(sim, 3) > 0:
             results.append({
                 "O*NET職業代碼": code,
                 "職業名稱": title,
@@ -108,17 +103,17 @@ for j in Data104:
                 "語意相似度": round(sim, 3),
                 "自動化風險分數": risk
             })
-        numerator += sim * risk
-        denominator += sim
+            numerator += sim * risk
+            denominator += sim
 
     # 計算加權總風險值
     weighted_risk = numerator / denominator if denominator != 0 else 0
     weighted_risk = round(weighted_risk, 3)
 
     # 判斷風險等級
-    if weighted_risk <= 0.33:
+    if weighted_risk <= low_level:
         risk_level = "低風險"
-    elif weighted_risk <= 0.66:
+    elif weighted_risk <= medium_level:
         risk_level = "中風險"
     else:
         risk_level = "高風險"
@@ -134,9 +129,9 @@ for j in Data104:
 
     results_104.append({
         "ID":uuid_string,
-        "公司": j[1],
-        "職務": j[0],
-        "工作內容":j[9],
+        "公司": row[1],
+        "職缺名稱": row[0],
+        "工作內容": row[9],
         "風險等級": risk_level,
         "自動化風險分數": weighted_risk
     })
@@ -147,15 +142,13 @@ for j in Data104:
 
     print(f"加權總自動化風險值：{weighted_risk} ({risk_level})")
     print(df_results)
-    str_data = j[0].replace(" ", "_")
-    str_data = str_data.replace("/", "_")
-    df_results.to_csv('output/'+ uuid_string +'.csv')
+    df_results.to_csv('output/'+ uuid_string +'.csv', index=False, encoding='utf-8-sig')
  
     
     try:
         with open(result_path, 'r') as f:
             # 檔案已存在，附加時不寫入表頭
-            df_results_104.to_csv(result_path, mode='a', header=False, index=False)
+            df_results_104.to_csv(result_path, mode='a', header=False, index=False, encoding='utf-8-sig')
     except FileNotFoundError:
         # 檔案不存在，第一次寫入，包含表頭
-        df_results_104.to_csv(result_path, mode='w', header=True, index=False)
+        df_results_104.to_csv(result_path, mode='w', header=True, index=False, encoding='utf-8-sig')
